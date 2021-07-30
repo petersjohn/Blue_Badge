88bf355e36caf218496daa6f169dd42a84cac866
John Peters <petersjohn0109@gmail.com>
2021-07-29 22:19:08 -0400

    RR Initial Commit

---
+++ Restaurant_Rater/RestaurantRater/RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,38 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
+        //post
+        //api/Restaurant
+        [HttpPost] //this eliminates the need for naming conventions on post, since .NET doesn't have to assume
+        public async Task<IHttpActionResult> CreateRestaurant([FromBody]Restaurant model)
+        {
+            if(model is null)
+            {
+                return BadRequest("Your request body cannot be empty");
+            }
+            if (ModelState.IsValid)
+            {
+                //store the model in the db
+                _context.Restaurants.Add(model);
+                await _context.SaveChangesAsync(); //this returns an int count of all the changes that were made
+                                                   //so in theory you could do "int changeCnt = await _context.SaveC....
+                                                   //and use this later for validation if you care.
+                return Ok("Your restaurant was saved!");
+            }
+            //if the model is not valid, reject it
+            return BadRequest(ModelState);
+        }
+
+    }
+}
---
+++ Restaurant_Rater/RestaurantRater/RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        //this is what the db table we set up in teh RestaurantDbContext.cs for restaurant is going to look like
+        [Key]
+        public int Id { get; set; }
+        [Required]
+
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+
+    }
+}

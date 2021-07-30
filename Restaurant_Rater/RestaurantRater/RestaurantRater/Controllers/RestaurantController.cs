33f0bad15fa14149cea3df34d1575943e959fdab
John Peters <petersjohn0109@gmail.com>
2021-07-30 08:48:58 -0400

    Updating changes to Restaurant controller

--- Restaurant_Rater/RestaurantRater/RestaurantRater/Controllers/RestaurantController.cs
+++ Restaurant_Rater/RestaurantRater/RestaurantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace RestaurantRater.Controllers
-{
+{   //Left off at https://elevenfifty.instructure.com/courses/749/pages/rr-api-2-dot-03-delete-functionality?module_item_id=65350
     public class RestaurantController : ApiController
     {
         private readonly RestaurantDbContext _context = new RestaurantDbContext();
@@ -33,6 +34,61 @@ namespace RestaurantRater.Controllers
             //if the model is not valid, reject it
             return BadRequest(ModelState);
         }
+        //GET ALL
+        //api/Restaurant
 
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {   
+            
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+        }
+
+        //GET BY ID
+        //api/Restaurant/{id}
+        [HttpGet]
+
+        public async Task<IHttpActionResult> GetByID([FromUri] int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if(restaurant is null)
+            {
+                return NotFound();
+            }
+            return Ok(restaurant);
+        }
+
+        //PUT (update)
+        // api/Restaurant/{id}
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateRestaurant([FromUri]int id, [FromBody] Restaurant updatedRestaurant)
+        {
+            //Check the IDs to see if the match
+            if(id != updatedRestaurant?.Id) //? this prevents an exception if the model returned is null; you can't access the id of a null object!(duh)
+            {
+                return BadRequest("Ids do not match");
+            }
+            //Check the model state
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            //Find the restaurant in the db
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            //if not found, then do a thing
+            if (restaurant is null)
+                return NotFound();
+
+            //update the properties
+            restaurant.Name = updatedRestaurant.Name;
+            restaurant.Address = updatedRestaurant.Address;
+            restaurant.Rating = updatedRestaurant.Rating;
+
+            //Save the changes
+            await _context.SaveChangesAsync();
+            return Ok("Update successful!");
+        }
     }
 }
